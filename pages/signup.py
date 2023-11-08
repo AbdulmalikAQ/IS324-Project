7bc536a4a8e1b2af627bd73d7e82c115da718a47
Salem Bahallah <zzzsamorayzzz@gmail.com>
2023-11-09 00:30:41 +0300

    initial commit

--- pages/signup.py
+++ pages/signup.py
@@ -47,7 +47,27 @@ class SignUp:
         tk.Button(self.frame, text="Login", width=10, command=self.login).place(x=295, y=347)
 
     def submit(self):
-        messagebox.showerror(title="Not available yet", message="Wait for the next version")
+        if self.first_name.get() == "" and self.last_name=="":
+            return messagebox.showerror(title="Missing Data", message="You must write your full name.")
+
+        if not self.id.get().isdigit():
+            return messagebox.showerror(title="Wrong Data", message="ID must be digits only.")
+
+        if len(self.id.get()) != 10 and len(self.id.get()) != 6:
+            return messagebox.showerror(title="Wrong Data", message="ID must be 10 or 6 digits only.")
+
+        if len(self.Password.get()) < 6:
+            return messagebox.showerror(title="Wrong Data", message="Password must be at least 6 digits or letters")
+
+        if not self.EmailAddress.get().endswith("@ksu.edu.sa"):
+            return messagebox.showerror(title="Wrong Data", message="Email address must ends with '@ksu.edu.sa'")
+
+        if not self.phoneNumber.get().startswith("05"):
+            return messagebox.showerror(title="Wrong Data",message="Phone number must starts with 05.")
+
+        if len(self.phoneNumber.get()) != 10:
+            return messagebox.showerror(title="Wrong Data", message="Phone number must have 10 digits only.")
+
 
     def login(self):
         self.main.change_page("login")
